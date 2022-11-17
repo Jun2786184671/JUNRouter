f268c748e4124313ba67f27152ab1c475d71630c
Jun Ma <maxinchun5@gmail.com>
2022-11-17 16:38:11 +0800

    Fixed a ui bug.

--- JUNRouter.podspec
+++ JUNRouter.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'JUNRouter'
-  s.version          = '0.1.1'
+  s.version          = '0.1.2'
   s.summary          = 'A simple intra-application routing framework.'
 
 # This description is used to generate tags and improve search results.
--- JUNRouter/Classes/Default/JUNDefaultRouter.m
+++ JUNRouter/Classes/Default/JUNDefaultRouter.m
@@ -19,7 +19,6 @@
 
 @implementation JUNDefaultRouter
 
-
 - (instancetype)initWithRouteMapper:(NSDictionary<NSString *, NSString *> *)routeMapper animated:(Boolean)animated routeExpress:(JUNRouteExpress *)routeExpress {
     if (self = [super init]) {
         _animated = animated;
@@ -39,9 +38,11 @@
     *cursor += 1;
     id<JUNRouter> prevRouter = self.routeExpress.currentRouter;
     id<JUNRouter> nextRouter = [[NSClassFromString(routerClsName) alloc] init];
-    [self _handleTransitionFrom:prevRouter to:nextRouter completion:^{
+    if ([[prevRouter class] isEqual:[nextRouter class]]) {
         next(nextRouter);
-    }];
+        return;
+    }
+    [self _handleTransitionFrom:prevRouter to:nextRouter nextHandler:next];
 }
 
 //- (void)jun_routeBuild:(JUNRouteBuilder *)route {
@@ -56,19 +57,19 @@
 //    }];
 //}
 
-- (void)_handleTransitionFrom:(id<JUNRouter>)prevRouter to:(id<JUNRouter>)nextRouter completion:(void (^)(void))completion {
+- (void)_handleTransitionFrom:(id<JUNRouter>)prevRouter to:(id<JUNRouter>)nextRouter nextHandler:(JUNRouterNextHandler)next {
     if (nextRouter == nil || prevRouter == nil) return;
     if (![nextRouter isKindOfClass:[UIViewController class]]) return;
     dispatch_async(dispatch_get_main_queue(), ^{
         if ([prevRouter isKindOfClass:[UIViewController class]]) {
-            [self _handleVcTransitionFrom:(UIViewController *)prevRouter to:(UIViewController *)nextRouter completion:completion];
+            [self _handleVcTransitionFrom:(UIViewController *)prevRouter to:(UIViewController *)nextRouter nextHandler:next];
             return;
         }
-        [self _presentVcToFront:(UIViewController *)nextRouter completion:completion];
+        [self _presentVcToFront:(UIViewController *)nextRouter nextHandler:next];
     });
 }
 
-- (void)_handleVcTransitionFrom:(UIViewController *)prevVc to:(UIViewController *)nextVc completion:(void (^)(void))completion {
+- (void)_handleVcTransitionFrom:(UIViewController *)prevVc to:(UIViewController *)nextVc nextHandler:(JUNRouterNextHandler)next {
     if ([prevVc isKindOfClass:[UITabBarController class]]) {
         UITabBarController *tabVc = (UITabBarController *)prevVc;
         for (UIViewController *vc in tabVc.childViewControllers) {
@@ -77,27 +78,40 @@
                 visibleVc = ((UINavigationController *)vc).visibleViewController;
             }
             if (![[visibleVc class] isEqual:[nextVc class]]) continue;
-            [tabVc jun_setSelectedViewController:vc completion:completion];
+            [tabVc jun_setSelectedViewController:vc completion:^{
+                next(visibleVc);
+            }];
             return;
         }
-        [self _handleVcTransitionFrom:tabVc.selectedViewController to:nextVc completion:completion];
+        [self _handleVcTransitionFrom:tabVc.selectedViewController to:nextVc nextHandler:next];
     } else if ([prevVc isKindOfClass:[UINavigationController class]]) {
-        [(UINavigationController *)prevVc jun_pushViewController:nextVc animated:self.isAnimated completion:completion];
+        UIViewController *visibleVc = ((UINavigationController *)prevVc).visibleViewController;
+        if ([[visibleVc class] isEqual:[nextVc class]]) {
+            next(nextVc);
+            return;
+        }
+        [(UINavigationController *)prevVc jun_pushViewController:nextVc animated:self.isAnimated completion:^{
+            next(nextVc);
+        }];
     } else if (prevVc.navigationController != nil) {
-        [prevVc.navigationController jun_pushViewController:nextVc animated:self.isAnimated completion:completion];
+        [prevVc.navigationController jun_pushViewController:nextVc animated:self.isAnimated completion:^{
+            next(nextVc);
+        }];
     } else if (prevVc.isBeingPresented || prevVc.presentingViewController != nil) {
         UIViewController *ancestor = prevVc.presentingViewController;
         [prevVc dismissViewControllerAnimated:self.isAnimated completion:^{
-            [self _handleVcTransitionFrom:ancestor to:nextVc completion:completion];
+            [self _handleVcTransitionFrom:ancestor to:nextVc nextHandler:next];
         }];
     } else {
-        [self _presentVcToFront:nextVc completion:completion];
+        [self _presentVcToFront:nextVc nextHandler:next];
     }
 }
 
-- (void)_presentVcToFront:(UIViewController *)vc completion:(void (^)(void))completion {
+- (void)_presentVcToFront:(UIViewController *)vc nextHandler:(JUNRouterNextHandler)next {
     UIViewController *rootVc = [UIApplication sharedApplication].keyWindow.rootViewController;
-    [rootVc presentViewController:vc animated:self.isAnimated completion:completion];
+    [rootVc presentViewController:vc animated:self.isAnimated completion:^{
+        next(vc);
+    }];
 }
 
 @end
